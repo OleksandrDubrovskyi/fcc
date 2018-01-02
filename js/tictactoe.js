20251d3f8a88f1c64a1159f24086965bb5786f97
Oleksandr Dubrovskyi <dubrovsky@live.com>
2018-01-02 23:00:20 +0200

    more functions

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,31 +1,43 @@
 const cells = Array.from(document.getElementsByTagName("td"));
 let emptyCells = ['n0', 'n1', 'n2', 'n3', 'n4', 'n5', 'n6', 'n7', 'n8'];
-let cellNumber = -1;
 
-
-for (let i = 0; i < cells.length; i++) {
-    cells[i].innerText = "";
-    cells[i].addEventListener("click", function(event){
-	    event.preventDefault();
-	    if(this.innerText === "") {
-	    	this.innerText = "0";
-	    	deleteItem(this.getAttribute('id'));
-	    	//cellNumber = this.getAttribute('id'); 
-	    }
-	    else {
-	    	this.innerText = "Fault"
-	    }
-	}); 
+function playGame() {
+	for (let i = 0; i < cells.length; i+=2) {
+	    humanMove();
+	    computerMove();
+	}	
 }
 
-function computerMove() {
 
+//function humanMove() {
+	for (let i = 0; i < cells.length; i++) {
+	    //cells[i].innerText = "";
+	    cells[i].addEventListener("click", function(event){
+		    event.preventDefault();
+		    if(this.innerText === "") {
+		    	this.innerText = "0";
+		    	deleteItem(this.getAttribute('id'));
+		    }
+		    else {
+		    	this.innerText = "Fault"
+		    }
+		    let compMove = computerMove();
+		    document.getElementById(emptyCells[compMove]).innerText = "X";
+		}); 
+	}
+//}
+
+function computerMove() {
+	
+	let move = Math.floor(Math.random(emptyCells.length)*emptyCells.length);
+	//document.getElementById(emptyCells[move]).innerText = "X";
+	deleteItem(emptyCells[move]);
+	return move;
 }
 
 function deleteItem(item) {
-	//console.log()
 	emptyCells.splice(emptyCells.indexOf(item), 1);
 	console.log(emptyCells);
-	//delete emptyCells[item];
-	//emptyCells = emptyCells.filter(function(n){ return n != undefined });
 }
+
+//playGame();
